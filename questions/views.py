8315202e477d05edd784168b07400d946356d58c
GyuReeKim <starkim96@naver.com>
2019-10-16 09:27:55 +0900

    index 기본구조

--- questions/views.py
+++ questions/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    pass
+    # 나중에 있을 충돌을 방지하기 위해 templates 안에 폴더를 따로 만들어준다.
+    return render(request, 'questions/index.html')
