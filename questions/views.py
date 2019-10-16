7c750081e585d62ed2f0de4556c55a52863f6483
GyuReeKim <starkim96@naver.com>
2019-10-16 10:47:56 +0900

    update-POST 완성

--- questions/views.py
+++ questions/views.py
@@ -50,7 +50,10 @@ def create(request):
 def update(request, id):
     question = get_object_or_404(Question, id=id)
     if request.method == "POST":
-        pass
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            form.save()
+            return redirect('questions:detail', id)
     else:
         form = QuestionForm(instance=question)
     context = {
