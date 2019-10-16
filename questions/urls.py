f0fc528e93aaec7f656ae77c694b7f0e2051949e
GyuReeKim <starkim96@naver.com>
2019-10-16 09:50:10 +0900

    Question 모델폼 생성, create-GET 요청 완성

--- questions/urls.py
+++ questions/urls.py
@@ -5,4 +5,5 @@ app_name = "questions"
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('create/', views.create, name="create"),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .forms import QuestionForm
 
 # Create your views here.
 def index(request):
     # 나중에 있을 충돌을 방지하기 위해 templates 안에 폴더를 따로 만들어준다.
     return render(request, 'questions/index.html')
+
+def create(request):
+    if request.method == "POST":
+        pass
+    else:
+        form = QuestionForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'questions/form.html', context)
+        # return render(request, 'questions/form.html', {'form': form,})
