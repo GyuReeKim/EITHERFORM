cba5fb0a1d2405334e28fcc3823573226fcfe002
GyuReeKim <starkim96@naver.com>
2019-10-16 11:11:15 +0900

    delete 완성

--- questions/urls.py
+++ questions/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('create/', views.create, name="create"),
     path('<int:id>/', views.detail, name="detail"),
     path('<int:id>/update/', views.update, name="update"),
+    path('<int:id>/delete/', views.delete, name="delete"),
 ]
